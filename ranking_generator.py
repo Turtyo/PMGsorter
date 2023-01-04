c3281533346747ce9b8a5a18ee2cdd39e76d7595
Turtyo <nathandiss09@gmail.com>
2023-01-04 20:22:32 +0100

    added a function to generate random ranking for activities by each student

---
+++ ranking_generator.py
@@ -0,0 +1,14 @@
+import random as rd
+
+def ranking_gen(number_of_students,number_of_activities):
+    """
+    Returns a random ranking matrix for a given number of student and activities
+    This matrix is made with the README convention, so there is no need to sort it by activities
+    """
+    choices = [j + 1 for j in range(number_of_activities)]
+    ranking=[]
+    for i in range(number_of_students):
+        for j in range(number_of_activities):
+            ranking.append(rd.shuffle(choices))
+    return ranking
+
