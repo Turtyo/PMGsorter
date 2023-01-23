5febaf01ea4d39dd002541e9eac4b13b6fabd351
Turtyo <nathandiss09@gmail.com>
2023-01-23 11:20:27 +0100

    define names for padding functions

---
+++ padding.py
@@ -0,0 +1,21 @@
+### Import
+
+### Functions
+
+def define_paddings():
+    return [random_padding,linear_padding,max_value_padding,min_value_padding]
+
+def select_padding_type():
+    return 0
+
+def random_padding():
+    return 0
+
+def linear_padding():
+    return 0
+
+def max_value_padding():
+    return 0
+
+def min_value_padding():
+    return 0
