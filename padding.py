75ec750b7e77e0a0d182e434e24c32d0ad2441f7
Turtyo <nathandiss09@gmail.com>
2023-01-24 21:48:13 +0100

    [functions] defined the functions to call when padding a matrix from outside the padding file

--- padding.py
+++ padding.py
@@ -18,4 +18,24 @@ def linear_padding(matrix_to_pad : np.ndarray) -> Padded_Matrix :
 def max_value_padding(matrix_to_pad : np.ndarray) -> Padded_Matrix :
 def min_value_padding(matrix_to_pad : np.ndarray) -> Padded_Matrix :
 def pad_matrix(padding_name : str, matrix_to_pad : np.ndarray) -> Padded_Matrix :
+    """
+    Return : matrix_to_pad padded with the chosen padding type
+    the matrix should be numpy
+    """
+    dict_of_padding = define_paddings()
+    return select_padding_type(padding_name, dict_of_padding)(matrix_to_pad)
+
 def padding_main(matrix_to_pad : np.ndarray, padding_name : str = 'linear', interactive_mode : bool = True) -> Padded_Matrix :
+    dict_of_padding = define_paddings()
+    _padding_name = padding_name
+    
+    if(interactive_mode):
+        _padding_name = ""
+        print("Here are the paddings you can choose")
+        paddings = dict_of_padding.keys
+        for name in paddings :
+            print(name)
+        while(_padding_name not in paddings):
+            _padding_name = input("Please choose one of the paddings : ")
+            
+    return pad_matrix(_padding_name, matrix_to_pad)
